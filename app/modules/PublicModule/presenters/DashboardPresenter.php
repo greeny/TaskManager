be0a45dcf78b476592e513db782c6fbcb3129572
greeny <blatny.tomas@seznam.cz>
2013-10-09 17:32:14 +0200

    Uploaded for debugging

--- app/modules/PublicModule/presenters/DashboardPresenter.php
+++ app/modules/PublicModule/presenters/DashboardPresenter.php
@@ -4,6 +4,24 @@
  */
 namespace TaskManager\PublicModule;
 
-class DashboardPresenter extends BasePublicPresenter {
+use Nette\Application\UI\Form;
 
+class DashboardPresenter extends BasePublicPresenter {
+	public function createComponentSendChatMessageForm()
+	{
+		$form = new Form;
+
+		$form->addTextArea('message', 'Zpráva:')
+			->setRequired('Prosím vyplň zprávu.');
+
+		$form->addSubmit('send', 'Poslat');
+
+		$form->onSuccess[] = $this->sendChatMessageFormSuccess;
+		return $form;
+	}
+
+	public function sendChatMessageFormSuccess(Form $form)
+	{
+		$v = $form->getValues();
+	}
 }
