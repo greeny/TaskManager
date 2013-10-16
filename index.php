b682a11c0bf7e1f109ac4b501078edf02267be33
greeny <blatny.tomas@seznam.cz>
2013-10-16 10:26:45 +0200

    Added possibility to take the site down

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 define("ROOT_PATH", __DIR__);
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-// require '.maintenance.php';
+if(file_exists('.maintenance')) require '.maintenance.php';
 
 // Let bootstrap create Dependency Injection container.
 $container = require __DIR__ . '/app/bootstrap.php';
